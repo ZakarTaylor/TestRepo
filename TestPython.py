707e1234ab9ce8458daae57e53317afbcedd99d0
Zakar <127263387+ZakarTaylor@users.noreply.github.com>
2025-12-12 16:54:36 -0500

    Added new input() events
    - Made the input() non-case sensitive
    - Added new input() option that gives you a list of numbers
    - Added message when no input is given

--- TestPython.py
+++ TestPython.py
@@ -1,5 +1,10 @@
-question = input("Who ate the last cookie?!!?!?!")
-if question == "I did.":
-  print("Okay, i respect that.")
-else:
-  print("I DONT LIKE YOUR TONE PAL!!")
+def change(amount):
+    if amount % 5 == 0: return [5] * (amount // 5)
+    elif amount % 7 == 0: return [7] * (amount // 7)
+    else: return change(amount - 5) + [5]
+
+question = input("Who ate the last cookie?!!?!?!").lower()
+if question == "i did" or question == "me": print("Okay, i respect that.")
+elif question == "forget that whats the winning numbers": print(change(256))
+elif question == "": print("Fine be that way.")
+else: print("I DONT LIKE YOUR TONE PAL!!")
